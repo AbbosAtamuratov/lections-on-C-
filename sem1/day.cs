1595f5a18f2bd304e05bc0f47738750707df8029
Like_Abbos <atammura.shin@gmail.com>
2022-08-04 21:30:19 +0300

    2 more exercises

---
+++ sem1/day.cs
@@ -0,0 +1,30 @@
+Console.Write("Введите число от 1 до 7 (получите день недели): ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 1)
+{
+    Console.WriteLine("Monday");
+}
+if (number == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+if (number == 3)
+{
+    Console.WriteLine("Wednsday");
+}
+if (number == 4)
+{
+    Console.WriteLine("Thursday");
+}
+if (number == 5)
+{
+    Console.WriteLine("Friday");
+}
+if (number == 6)
+{
+    Console.WriteLine("Saturday");
+}
+if (number == 7)
+{
+    Console.WriteLine("Sunday");
+}
---
+++ sem1/is it sqr.cs
@@ -0,0 +1,10 @@
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a * a == b)
+{
+    Console.WriteLine(b + " квадрат " + a);
+}
+else
+{
+    Console.WriteLine(b + " не квадрат " + a);
+}
