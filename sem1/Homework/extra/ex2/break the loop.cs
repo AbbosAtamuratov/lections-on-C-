a1684f384aa914642178e2fbcb3b54a9e09dd7ca
Like_Abbos <atammura.shin@gmail.com>
2022-08-06 18:32:33 +0300

    extra 2 ready

---
+++ sem1/Homework/extra/ex2/break the loop.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Напиши ВЫХОД по-английски: ");
+string input = "googoo gaga";
+while (input != "exit")
+{
+    input = Console.ReadLine();
+    if (input == "exit")
+    {
+        break;
+    }
+    else
+    {    
+        Console.WriteLine("Неправильно!");
+    }
+}
