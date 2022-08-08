4ab8d482484951023ed0e0bfff07d1aea75f37a5
Like_Abbos <atammura.shin@gmail.com>
2022-08-09 00:56:17 +0300

    sem2 hw ex2 problem

---
+++ sem1/sem2/homework/ex2/Find3rdDigit.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+string ReadStr(string message)
+{
+    Console.Write(message);
+    return Console.ReadLine();
+}
+
+bool isItBigEnough(string a)
+{
+    return a.Length > 2;
+}
+
+char thirdDigit (string word)
+{
+    int size = word.Length;
+    char res = '1';
+    int diff = size-3;
+    res = Convert.ToChar(word.Substring(word.Length-diff));
+    return res;
+} 
+
+string input = ReadStr("Введите число: ");
+Console.Write(thirdDigit(input));
