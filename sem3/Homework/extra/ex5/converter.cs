ea9a4a30a4a322caacac4c3194d76b1fce89a16d
Like_Abbos <atammura.shin@gmail.com>
2022-08-13 09:40:57 +0300

    all commands are working, need to add single convertations

--- sem3/Homework/extra/ex5/converter.cs
+++ sem3/Homework/extra/ex5/converter.cs
@@ -3,10 +3,10 @@
 // Курс конвертации просто описать в программе. 
 // Программа заканчивает свою работу в тот момент, когда решит пользователь.
 
-int ReadInt(string message)
+double ReadDbl(string message)
 {
     Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
+    return Convert.ToDouble(Console.ReadLine());
 }
 
 string[] currensy = new string[] { "Алтын", "Гульден", "Тугрик" };
@@ -14,26 +14,58 @@ string[] currensy = new string[] { "Алтын", "Гульден", "Тугрик
 // 1 гульден = 2 алтына = 20 тугриков
 // 1 тугрик = 0,1 алтын = 0,05 гульденов
 
-string printConvertationDirection(string a, string b)
+void showMY(double[] array)
 {
-    return a + " -> " + b;
+    Console.WriteLine($"Ваш баланс: {array[0]} ATN -> {array[1]} GDN -> {array[2]} TGK");
 }
 
-void Info (string[] currency)
+void Info(string[] currency)
 {
-    Console.WriteLine ("Список команд: ");
-    Console.WriteLine ("showALL - показывает доступные валюты.");
-    Console.WriteLine ("showMY - показывает баланс в текущей валюте.");
-    Console.WriteLine ($"read_ATN - позволяет ввести сумму в {currency[0]}ах.");
-    Console.WriteLine ($"read_GDN - позволяет ввести сумму в {currency[1]}ах.");
-    Console.WriteLine ($"read_TGK - позволяет ввести сумму в {currency[2]}ах.");
-    Console.WriteLine ($"ATNtoGDN- переводит ваш баланс из {currency[0]}ов в {currency[1]}ы.");
-    Console.WriteLine ($"ATNtoTGK - переводит ваш баланс из {currency[0]}ов в {currency[2]}и.");
-    Console.WriteLine ($"GDNtoATN - переводит ваш баланс из {currency[1]}ов в {currency[0]}ы.");
-    Console.WriteLine ($"GDNtoTGK - переводит ваш баланс из {currency[1]}ов в {currency[2]}и.");
-    Console.WriteLine ($"TGKtoATN - переводит ваш баланс из {currency[2]}ов в {currency[0]}ы.");
-    Console.WriteLine ($"TGKtoGDN - переводит ваш баланс из {currency[2]}ов в {currency[1]}ы.");
-    Console.WriteLine ("exit - завершает работу.");
+    Console.WriteLine("Список команд: ");
+    Console.WriteLine("showALL - показывает доступные валюты.");
+    Console.WriteLine("showMY - показывает баланс в каждой валюте.");
+    Console.WriteLine($"readATN - позволяет ввести сумму в {currency[0]}ах.");
+    Console.WriteLine($"readGDN - позволяет ввести сумму в {currency[1]}ах.");
+    Console.WriteLine($"readTGK - позволяет ввести сумму в {currency[2]}ах.");
+    Console.WriteLine("exit - завершает работу.");
+}
+
+double[] ReadATN(double balance)
+{
+    double[] res = new double[3];
+    res[0] = balance;
+    res[1] = balance * 0.5;
+    res[2] = balance * 10;
+    return res;
+}
+
+double[] ReadGDN(double balance)
+{
+    double[] res = new double[3];
+    res[0] = balance * 2;
+    res[1] = balance;
+    res[2] = balance * 20;
+    return res;
+}
+
+double[] ReadTGK(double balance)
+{
+    double[] res = new double[3];
+    res[0] = balance / 10;
+    res[1] = balance * 0.05;
+    res[2] = balance;
+    return res;
+}
+
+void showALL (string[] curr)
+{
+    Console.WriteLine ("");
+    Console.WriteLine ("Вам доступны следующие валюты: ");
+    Console.WriteLine ($"{curr[0]}, {curr[1]}, {curr[2]} ");
 }
 
 Info(currensy);
+double cash = ReadDbl("Введите ваш баланс: ");
+double[] converted = ReadTGK (cash);
+showALL(currensy);
+showMY(converted); 
