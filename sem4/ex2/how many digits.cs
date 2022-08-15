9be3bd1f85a3bcf700e34e5f9829a6bde7040af5
Like_Abbos <atammura.shin@gmail.com>
2022-08-15 20:52:24 +0300

    tech break

---
+++ sem4/ex2/how many digits.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// no string allowed
+ int ReadInt(string msg)
+ {
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+ }
+
+int number = ReadInt ("Введите число: ");
+int click = 0;
+while(number>0)
+{
+    number /= 10;
+    click++;
+}
+
+Console.Write($"В вашем числе {click} цифр.");
---
+++ sem4/ex3/rangeProduct.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int number = ReadInt("Введите число ");
+int result = 1;
+
+for (int i = 1; i <= number; i++)
+{
+    result *= i;
+}
+
+Console.WriteLine(result);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
