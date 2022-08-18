deece1bd146f7c22f424dd7b0bf88b71cccbbaf7
Like_Abbos <atammura.shin@gmail.com>
2022-08-18 18:31:40 +0300

    extra ex6 modified

--- sem4/homework/extra/ex6/ArrayJob.cs
+++ sem4/homework/extra/ex6/ArrayJob.cs
@@ -8,54 +8,56 @@ int ReadInt(string message)
     return Convert.ToInt32(Console.ReadLine());
 }
 
-void PrintArray(int[] a)
+void PrintArray(int[] array)
 {
-    for (int i = 0; i < a.Length; i++) 
-                Console.Write(a[i] + " ");
+    for (int i = 0; i < array.Length; i++) 
+         Console.Write(array[i] + " ");
     Console.WriteLine("");
 }
 
 int[] CreateArray(int size)
 {
     int[] a = new int[size];
-    for (int i = 0; i < a.Length; i++) a[i] = new Random().Next(0, 100);
+    for (int i = 0; i < a.Length; i++) 
+        a[i] = new Random().Next(0, 100);
     return a;
 }
 
-int[] AddToArray(int[] a)
+int[] AddToArray(int[] array, int value)
 {
-    int[] result = new int[a.Length + 1];
-    for (int i = 0; i < a.Length; i++) result[i] = a[i];
-    for (int j = a.Length; j < a.Length + 1; j++)
-    {
-        result[j] = new Random().Next(0, 100);
-    }
+    int[] result = new int[array.Length + 1];
+    for (int i = 0; i < array.Length; i++) 
+        result[i] = array[i];     
+    result[result.Length-1] = value;
     return result;
 }
 
-int FindTargetIndex(int[] b, int target)
+int FindTargetIndex(int[] array, int target)
 {
     int i=0;
-    while (b[i] != target && i < b.Length)
+    while (array[i] != target && i < array.Length)
     {
         i++;
     }
     return i;
 }
 
-int[] CutOutOfArray(int[] a)
+int[] CutOutOfArray(int[] array, int index)
 {
-    int target = ReadInt("Введите значение элемента, которое надо удалить: ");
-    int castawayIndex = FindTargetIndex(a, target);
-    int[] result = new int[a.Length - 1];
-    for (int i = 0; i<castawayIndex; i++) result[i] = a[i];
-    for (int j = castawayIndex; j<result.Length; j++) result[j] = a[j+1];
+    int[] result = new int[array.Length - 1];
+    for (int i = 0; i<index; i++) 
+        result[i] = array[i];
+    for (int j = index; j<result.Length; j++) result[j] = array[j+1];
     return result;
 }
 
 int initialSize = ReadInt("Введите размер случано генерируемого массива: ");
 int[] array = CreateArray(initialSize);
 PrintArray(array);
-int[] expanded = AddToArray(array); 
+int newNumber = new Random().Next(0, 100);
+int[] expanded = AddToArray(array,newNumber); 
+Console.WriteLine("Добавляем ячейку в массив:");
 PrintArray(expanded);
-PrintArray(CutOutOfArray(expanded));
+int target = ReadInt("Введите значение элемента, которое надо удалить: ");
+int castawayIndex = FindTargetIndex(expanded, target);
+PrintArray(CutOutOfArray(expanded, castawayIndex));
