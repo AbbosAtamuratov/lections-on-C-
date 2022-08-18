2194ce971a2b96beab081a8be740ab30699333f8
Like_Abbos <atammura.shin@gmail.com>
2022-08-18 22:58:02 +0300

    sem5 over

---
+++ sem5/ex4/countMe.cs
@@ -0,0 +1,37 @@
+﻿// Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, 
+// значения которых лежат в отрезке [10,99]. Пример для массива из 5, а не 123 элементов. 
+// В своём решении сделайте для 123
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 201);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int CountNeeded(int[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] < 100)
+            counter++;
+    }
+    return counter;
+}
+
+int[] numbers = new int[123];
+FillArrayRandomNumbers(numbers);
+int result = CountNeeded(numbers);
+PrintArray(numbers);
+Console.Write($"\nВ заданном массиве {result} чисел от 10 до 99");
---
+++ sem5/ex5/Array product.cs
@@ -0,0 +1,53 @@
+﻿// Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, 
+// второй и предпоследний и т.д. Результат запишите в новом массиве.
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArrayRandomNumbers(int[] array1)
+{
+    for (int i = 0; i < array1.Length; i++)
+        array1[i] = new Random().Next(0, 10);
+}
+
+void PrintArray(int[] array1)
+{
+    for (int i = 0; i < array1.Length; i++)
+        Console.Write(array1[i] + " ");
+    Console.WriteLine();
+}
+
+int GetSize(int[] array1)
+{
+    if (array1.Length %2 == 0)
+        return array1.Length/2;
+    else 
+        return (array1.Length/2)+1;
+}
+
+int[] ArrayProduct(int[] array1)
+{
+    int resSize = GetSize(array1);
+    int[] resultArray = new int[resSize];
+    if (array1.Length %2 == 0)
+    {
+        for (int i=0; i<resSize; i++)
+            resultArray[i] = array1[i] * array1[array1.Length-1-i];
+    }
+    else
+    {
+        for (int j=0; j<resSize; j++)
+            resultArray[j] = array1[j] * array1[array1.Length-1-j];
+        resultArray[resSize-1] = array1[((array1.Length-1)/2)];
+    }
+    return resultArray;
+}
+
+int size = ReadInt("Введите размер массива: ");
+int[] array = new int[size];
+FillArrayRandomNumbers(array);
+PrintArray(array);
+PrintArray(ArrayProduct(array));
