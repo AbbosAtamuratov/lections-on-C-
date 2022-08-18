13fd8847ff53965329ad048e9757ef453afda277
Like_Abbos <atammura.shin@gmail.com>
2022-08-18 21:01:56 +0300

    sem5 halfway through

---
+++ sem5/ex1/arraySums.cs
@@ -0,0 +1,35 @@
+﻿int size = 12;
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+int sumNegative = 0;
+int sumPositive = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+        sumPositive += numbers[i];
+    else
+        sumNegative += numbers[i];
+}
+
+Console.WriteLine($"Сумма положительных элементов равна {sumPositive}\n" +
+                   $"Сумма отрицательных равна {sumNegative}");
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
---
+++ sem5/ex3/findMe.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+int size = ReadInt("Введите размер массива: ");
+int[] array = new int[size];
+FillArrayRandomNumbers(array);
+PrintArray(array);
+int number = ReadInt("Введите число которое нужно найти: ");
+
+if (FindMe(array, number))
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
+
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArrayRandomNumbers(int[] array1)
+{
+    for (int i = 0; i < array1.Length; i++)
+        array1[i] = new Random().Next(-9, 10);
+}
+
+bool FindMe(int[] array1, int target)
+{
+    bool result = false;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i] == target) result = true;
+    }
+    return result;
+}
+
+void PrintArray(int[] array1)
+{
+    for (int i = 0; i < array1.Length; i++)
+        Console.Write(array1[i] + " ");
+    Console.WriteLine();
+}
