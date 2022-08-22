b5c0cb68512d5689bafdae03ad70369fc10f9391
Like_Abbos <atammura.shin@gmail.com>
2022-08-22 22:51:06 +0300

    sem6 over

---
+++ sem6/ex4/fibonacci.cs
@@ -0,0 +1,18 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+
+int number = ReadInt("Введите число: ");
+
+if(number == 1 || number == 2)
+{
+    Console.Write("Для ряда Фибоначчи нужно число больше 2 ");
+    return;
+}
+
+int[] fibonacci = new int[number];
+fibonacci[0] = 0;
+fibonacci[1] = 1;
+
+for (int i = 2; i < number; i++)
+{
+    fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];  
+}
---
+++ sem6/ex5/copyArray.cs
@@ -0,0 +1,33 @@
+﻿int size = ReadInt("Укажите длину массива: ");
+int[] array = new int[size];
+int[] newArray = new int[array.Length];
+
+FillArrayRandomNumbers(array);
+PrintArray(array);
+
+for(int i = 0; i < array.Length; i++)
+{
+    newArray[i] = array[i];
+}
+
+PrintArray(newArray);
+
+void FillArrayRandomNumbers(int[] array1)
+{
+    for (int i = 0; i < array1.Length; i++)
+        array1[i] = new Random().Next(0, 10);
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+void PrintArray(int[] array1)
+{
+    for (int i = 0; i < array1.Length; i++)
+        Console.Write(array1[i] + " ");
+    Console.WriteLine();
+}
