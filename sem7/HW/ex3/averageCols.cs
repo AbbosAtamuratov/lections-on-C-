d77bf361900da73df5c0b3b494b77118d508a560
Like_Abbos <atammura.shin@gmail.com>
2022-08-25 23:53:40 +0300

    polish extra 8 & finish hw3

--- sem7/HW/ex3/averageCols.cs
+++ sem7/HW/ex3/averageCols.cs
@@ -30,37 +30,37 @@ void PrintArray2D(int[,] array)
     Console.WriteLine();
 }
 
-double ColAverage(int[,] array, int colNumber)
-{
-    int sum = 0;
-    for (int i = 0; i < array.GetLength(1); i++)
-        sum += array[i, colNumber];
-    double avg = sum/ array.GetLength(1);
-    return avg;
-}
-
 /*-----------------MAIN--------------------------*/
 
+int rows = ReadInt("Введите количество строк (не больше 9): ");
+int cols = ReadInt("Введите количество столбцов (не больше 9): ");
 bool sizeRestrictions = false;
+
 while (!sizeRestrictions)
 {
-    Console.WriteLine("Некорректный ввод!");
-    rows = ReadInt("Введите количество строк (НЕ БОЛЬШЕ 9): ");
-    cols = ReadInt("Введите количество столбцов (НЕ БОЛЬШЕ 9): ");
     if (cols >= 0 && cols < 10 && rows >= 0 && rows < 10)
     {
         sizeRestrictions = true;
     }
+    else
+    {
+        Console.WriteLine("Некорректный ввод!");
+        rows = ReadInt("Введите количество строк (НЕ БОЛЬШЕ 9): ");
+        cols = ReadInt("Введите количество столбцов (НЕ БОЛЬШЕ 9): ");
+    }
 }
 
-int[,] numbers = new int [rows,cols];
+int[,] numbers = new int[rows, cols];
+
 FillArray2D(numbers);
 PrintArray2D(numbers);
 
+int[] Col;
 for (int i = 0; i < numbers.GetLength(0); i++)
 {
-    for (int j = 0; j < numbers.GetLength(1); j++)
-    {
-        Console.WriteLine ($"Среднее арифметическое {i+1}го столбца: {ColAverage(numbers,  j)}");
-    }
+    int sum = 0;
+    int currentCol=0;
+    
+//     double avg = sum / numbers.GetLength(1);
+//     Console.WriteLine ($"Среднее арифметическое столбца {currentCol+1} : {avg}");
 }
